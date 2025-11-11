ec6b7dbebac220db0ee2fba177461cf1f64ea67c
Julián Aponte <aponteapps@gmail.com>
2025-11-10 21:48:35 -0500

    Add files via upload

---
+++ inventario.py
@@ -0,0 +1,8 @@
+name = ('')
+price = 0.0
+quantity = 0
+
+name = str(input("Nombre del producto: "))
+price = float(input("Precio del producto: "))
+quantity = int(input("Cantidad del producto: "))
+
